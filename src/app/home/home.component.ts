d26e8e04b499b316de30fe6096295b9698b99d25
Fanchao92 <fanchaozhou1992@gmail.com>
2018-07-15 02:28:15 -0700

    Form data binding: text, checkbox, radio button and select

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,6 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+	public languages = ['English', 'Spanish', 'Chinese', 'Others'];
+	model = new Employee('Erick', 'Zhou', true, 'w2', 'English');
 }
